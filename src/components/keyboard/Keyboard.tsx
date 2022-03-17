ffc561093eb102d4c7433f941985eae4a330ae88
nabeelshar <sharyt26shar@gmail.com>
2022-03-17 10:54:46 +0500

    first commit

---
+++ src/components/keyboard/Keyboard.tsx
@@ -0,0 +1,108 @@
+import { getStatuses } from '../../lib/statuses'
+import { Key } from './Key'
+import { useEffect } from 'react'
+import { ENTER_TEXT, DELETE_TEXT } from '../../constants/strings'
+import { localeAwareUpperCase } from '../../lib/words'
+
+type Props = {
+  onChar: (value: string) => void
+  onDelete: () => void
+  onEnter: () => void
+  guesses: string[]
+  isRevealing?: boolean
+}
+
+export const Keyboard = ({
+  onChar,
+  onDelete,
+  onEnter,
+  guesses,
+  isRevealing,
+}: Props) => {
+  const charStatuses = getStatuses(guesses)
+
+  const onClick = (value: string) => {
+    if (value === 'ENTER') {
+      onEnter()
+    } else if (value === 'DELETE') {
+      onDelete()
+    } else {
+      onChar(value)
+    }
+  }
+
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.code === 'Enter') {
+        onEnter()
+      } else if (e.code === 'Backspace') {
+        onDelete()
+      } else {
+        const key = localeAwareUpperCase(e.key)
+        // TODO: check this test if the range works with non-english letters
+        if (key.length === 1 && key >= 'A' && key <= 'Z') {
+          onChar(key)
+        }
+      }
+    }
+    window.addEventListener('keyup', listener)
+    return () => {
+      window.removeEventListener('keyup', listener)
+    }
+  }, [onEnter, onDelete, onChar])
+
+  return (
+    <div>
+      <div className="flex justify-center mb-1">
+        {['ط','ض', 'ص', 'غ', 'ژ', 'ڑ', 'ٹ', 'ث', 'ح', 'ئ', 'ظ'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center mb-1">
+        {['پ','ق', 'و', 'ع', 'ر', 'ت', 'ے', 'ء', 'ی', 'ہ'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center mb-1">
+        {['ل','ا','آ', 'س', 'ڈ', 'د', 'ف', 'گ', 'ھ', 'ج', 'ک'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <Key width={65.4} value="ENTER" onClick={onClick}>
+          {ENTER_TEXT}
+        </Key>
+        {['ذ','ز','ش','خ','چ','ب','ں','ن','م'].map((key) => (
+          <Key
+            value={key}
+            key={key}
+            onClick={onClick}
+            status={charStatuses[key]}
+            isRevealing={isRevealing}
+          />
+        ))}
+        <Key width={65.4} value="DELETE" onClick={onClick}>
+          {DELETE_TEXT}
+        </Key>
+      </div>
+    </div>
+  )
+}
